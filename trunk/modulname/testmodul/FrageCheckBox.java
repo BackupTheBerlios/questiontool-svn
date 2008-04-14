092201eff28c9c1a4b6d32460d98fd4bb7cd3b10
riebenbc <riebenbc@02243042-5f3e-0410-9ad3-a21b4500cd43>
2008-04-14 01:36:32 +0000

    Fragebogenklasse und Fragenklassen (Vererbte - Checkbox, Dropdown, Noten, Radio, Text, TextArea)

---
+++ trunk/modulname/testmodul/FrageCheckBox.java
@@ -0,0 +1,17 @@
+
+public class FrageCheckBox extends Frage {
+
+	private final int typ = 2;
+	
+	public FrageCheckBox(String frage) {
+		super(frage);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getTyp() {
+		return typ;
+	}
+	
+	
+
+}
---
+++ trunk/modulname/testmodul/FrageText.java
@@ -0,0 +1,15 @@
+
+public class FrageText extends Frage {
+
+	private final int typ = 3;
+	
+	public FrageText(String frage) {
+		super(frage);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getTyp() {
+		return typ;
+	}
+
+}
